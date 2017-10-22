64dcc7eab757b887e0b4336f22f4dfd7d218fef7
Nathaniel Shimoni <nathanie@post.bgu.ac.il>
2017-10-23 00:23:44 +0300

    additional basic pre-processing
    in response to issue #3

--- convert_data_to_long_format.py
+++ convert_data_to_long_format.py
@@ -6,5 +6,9 @@ tr_data.rename(columns={'Unnamed: 0':'id','class':'target'},inplace=True)
 train_features = [f for f in tr_data.columns if ('Time' not in f)and('target' not in f)]
 temp = tr_data.loc[:,train_features]
 temp.fillna(-999,inplace=True)
-long_tr_data = temp.melt(id_vars='id',value_vars=train_features[1:])
+temp = temp.melt(id_vars='id',value_vars=train_features[1:])
+temp['feature'] = [x[:4] for x in temp['variable']]
+temp['timeStamp'] = [int(x.split('_')[1]) for x in temp['variable']]
+long_data_format = temp.pivot_table(index=['id','timeStamp'],columns='feature',values='value',aggfunc=sum)
+print(long_data_format)
 del temp
